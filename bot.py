7a17c8932b1a0154934f3abb6fe9aa2d4dc5e4ff
Chuba Samuel <chubasamuel@gmail.com>
2020-03-14 19:25:34 +0100

    ---__---

--- bot.py
+++ bot.py
@@ -119,6 +119,8 @@ def showHelp(bot,update):
     msg+="e.g\n   capt=Paediatric Textbook 4th Ed. | https://www.xz.bxxbbd.didj/jdjjf/paed.pdf\n"
     msg+="or e.g\n   capt=Anatomy Text| http://book.bk/jdjdhhdnd/anatomy.pdf/jdjdjjdeojrnfnf"
     update.message.reply_text(msg)
+def cAll(bot,update):
+    update.message.reply_text(str(update))
 if __name__ == '__main__':
     logger.info("Starting bot")
     updater = Updater(TOKEN)
@@ -128,6 +130,7 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('help',showHelp))
     dp.add_handler(CommandHandler('start',startBot))
     dp.add_handler(MessageHandler(Filters.update.channel_post,cEcho))
+    dp.add_handler(MessageHandler(Filters.update.all,cAll))
     dp.add_handler(MessageHandler(Filters.document,file_id_gt))
     dp.add_handler(MessageHandler(Filters.update.message&(Filters.entity(MessageEntity.URL)|Filters.entity(MessageEntity.TEXT_LINK)),file_id_gt2))
     dp.add_handler(MessageHandler(Filters.update.channel_posts&Filters.regex(r'^@dcorbot\s+capt=.+\|\s+https{0,1}://.+(pdf|ppt|xls|xlsx|html|pptx|txt|doc|docx|epub)$'),channel_getBookByUrl))
